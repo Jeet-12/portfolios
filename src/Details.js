e0c607205bb0a6d5f879352a0db6811532058a5c
Jeet <dewanganjd2003@gmail.com>
2024-07-26 02:39:58 +0530

    main

---
+++ src/Details.js
@@ -0,0 +1,177 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/face.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.png";
+import projectImage2 from "./assets/projects/project2.png";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.avif";
+import projectImage5 from "./assets/projects/project5.png";
+import projectImage6 from "./assets/projects/project6.png";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Jeet Dewangan",
+  tagline: "I build things for Web & App",
+  img: profile,
+  about: `I'm a proficient web developer from Raipur, Durg, with extensive experience in multiple technologies like React JS, PHP, and MERN. And notable achievements include significant improvements in website performance and search engine visibility, along with a strong educational background in B.Tech from Chhatrapati Shivaji Institute of Technology.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/thejeetdewangan1/",
+  github: "https://github.com/Jeet-12",
+  twitter: "https://x.com/JEETDEW46640489",
+  instagram: "https://www.instagram.com/the_real_jeet_?igsh=bDZiYXk4cTQzcHc1",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Web Developer Intern",
+    Company: `Pylons Technology`,
+    Location: "Durg",
+    Type: "Internship",
+    Duration: "Jan 2022 - April 2022",
+  },
+  {
+    Position: "Web Developer Internship",
+    Company: `The Digi Jugad`,
+    Location: "Durg",
+    Type: "Internship",
+    Duration: "JUne 2022 - Oct 2022",
+  },
+  {
+    Position: "Web Developer Internship",
+    Company: `Logixhunt`,
+    Location: "Durg",
+    Type: "Internship",
+    Duration: "Feb 2023 - Apr 2023",
+  },
+  {
+    Position: "Software Developer ",
+    Company: `Techcherry Software`,
+    Location: "Durg",
+    Type: "Full Time",
+    Duration: "July 2023 - Nov 2023",
+  },
+  {
+    Position: "Backend Developer ",
+    Company: `BetterSpace`,
+    Location: "Raipur",
+    Type: "Full Time",
+    Duration: "Feb 2024 - Present",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  
+  {
+    Position: "Bachelor in Computer Science And Engineering",
+    Company: `Chhatrapati Shivaji Institute Of Technology`,
+    Location: "Durg",
+    Type: "Full Time",
+    Duration: "Sep 2020 - May 2024",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Project title 1",
+    image: projectImage1,
+    description: `Indulge in a visually stunning food website featuring vibrant images, sleek design, and easy navigation. Discover mouthwatering recipes, restaurant reviews, and culinary tips in an elegant, user-friendly interface.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://foodiejeet.vercel.app/",
+    githubLink: "https://github.com/Jeet-12/foodiejeet",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `A comprehensive real estate website offering property listings, virtual tours, market insights, and expert advice. User-friendly interface for buying, selling, and renting homes with advanced search and personalized recommendations.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://realstate-lac.vercel.app/",
+    githubLink: "https://github.com/Jeet-12/realstate",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `Our chat application website offers seamless, real-time messaging with rich features like file sharing, group chats, and end-to-end encryption, ensuring secure and efficient communication for personal and professional use.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://github.com/Jeet-12/gupshup",
+    githubLink: "https://github.com/Jeet-12/gupshup",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `An eCommerce website offering a seamless shopping experience with a vast selection of products, secure payment options, user-friendly navigation, personalized recommendations, and fast, reliable delivery services.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://github.com/Jeet-12/Ecomwithpayment",
+    githubLink: "https://github.com/Jeet-12/Ecomwithpayment",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `Discover top travel destinations, book affordable tours, and find exclusive deals on our user-friendly website. Your adventure begins here with personalized itineraries and 24/7 customer support for a seamless experience.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://tour-travel-eight.vercel.app/",
+    githubLink: "https://github.com/Jeet-12/tour-travel",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `A comprehensive education website offering interactive courses, engaging tutorials, and a wide range of resources for students and educators. Empowering learning with expert guidance and innovative tools for academic success.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://github.com/Jeet-12/education",
+    githubLink: "https://github.com/Jeet-12/education",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "dewanganjd.1@gmail.com",
+  phone: "+91 9131814057",
+};
